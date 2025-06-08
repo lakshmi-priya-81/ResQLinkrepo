226cdb44cfea9e06141f321dd290d669298a2e83
Shree Lakshmi Priya <71762307050@cit.edu.in>
2025-06-08 20:19:37 +0530

    fix: add image domains to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -4,3 +4,18 @@ const nextConfig = {
 };
 
 export default nextConfig;
+// next.config.mjs
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'img.freepik.com',
+      'www.careinsurance.com',
+      'www.pngarc.com',
+      'www.shutterstock.com',
+    ],
+  },
+};
+
+export default nextConfig;
